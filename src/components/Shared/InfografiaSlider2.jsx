0dc300f1e4653859ee37356677f71fb1bac323bf
Yhonniel Javier Suárez Bellorin <yhs@iMac-de-Yhonniel.local>
2022-10-07 13:02:48 -0500

    cambios

--- src/components/Shared/InfografiaSlider2.jsx
+++ src/components/Shared/InfografiaSlider2.jsx
@@ -4,68 +4,55 @@ import "../Shared/index.css";
 const InfografiaSlider2 = () => {
   return (
     <>
-      <section className='mb-5 py-0 mt-5'>
+      <section className='blog-page py-5'>
         <div className='container'>
-          <div className='row testimonials'>
-            {/* <!--testimonials left--> */}
-            <div className='col-lg-6 col-md-12 px-4 py-5 p-lg-5'>
-              <div className='h-100'>
-                <div className='title p-0'>
-                  <span className=''>Objetivo de Desarrollo Sostenible 4 </span>
-                  <h4>
-                    Garantizar una educación inclusiva, equitativa y de calidad
-                    y promover oportunidades de aprendizaje durante toda la vida
-                    para todos.
-                  </h4>
+          <div className='col-md-8 mx-auto'>
+            <div className='post-item'>
+              <div className='post-item-wrap'>
+                <div className='post-item-desc'>
+                  <blockquote class='blockquote primary-line' cite='#'>
+                    <h3>
+                      Garantizar una educación inclusiva, equitativa y de
+                      calidad y promover oportunidades de aprendizaje durante
+                      toda la vida para todos.
+                    </h3>
+                  </blockquote>
                 </div>
-                <div className='row'>
-                  <div className='tiny-slider testi-full dots-dark'>
-                    <div
-                      className='tiny-slider-inner'
-                      data-arrow='false'
-                      data-items='1'
-                      data-autoplay='3800'
-                    >
-                      {/* <!-- testimonial item --> */}
-                      <div className='item'>
-                        <div className='testimonials-wrap'>
-                          {" "}
-                          <img
-                            src='/images/infografias/B.png'
-                            alt='infografias'
-                          />{" "}
-                        </div>
+
+                <div className='post-image tiny-slider testi-full dots-dark'>
+                  <div
+                    className='tiny-slider-inner'
+                    data-arrow='false'
+                    data-items='1'
+                    data-autoplay='3800'
+                  >
+                    <div className='item'>
+                      <div className='testimonials-wrap'>
+                        <img
+                          src='/images/infografias/B.png'
+                          alt='infografias'
+                        />{" "}
                       </div>
-                      <div className='item'>
-                        <div className='testimonials-wrap'>
-                          {" "}
-                          <img
-                            src='/images/infografias/C.png'
-                            alt='infografias'
-                          />{" "}
-                        </div>
+                    </div>
+                    <div className='item'>
+                      <div className='testimonials-wrap'>
+                        <img
+                          src='/images/infografias/C.png'
+                          alt='infografias'
+                        />{" "}
                       </div>
-                      <div className='item'>
-                        <div className='testimonials-wrap'>
-                          {" "}
-                          <img
-                            src='/images/infografias/D.png'
-                            alt='infografias'
-                          />{" "}
-                        </div>
+                    </div>
+                    <div className='item'>
+                      <div className='testimonials-wrap'>
+                        <img
+                          src='/images/infografias/D.png'
+                          alt='infografias'
+                        />{" "}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* <!--testimonials right--> */}
-            <div className='col-lg-6 d-none d-lg-block'>
-              <img
-                className='img-home-3'
-                src='/images/infografias/A.png'
-                alt='img'
-              />
             </div>
           </div>
         </div>
--- src/components/Shared/Intituciones.jsx
+++ src/components/Shared/Intituciones.jsx
@@ -2,23 +2,28 @@ import React from "react";
 
 const Intituciones = () => {
   return (
-    <section className='client py-0'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12'>
-            <h3 className='mt-5 display-5 fw-bold text-center'>
-              Instituciones Participantes
-            </h3>
-            <div>
-              <img
-                src='/images/instituciones/instituciones.png'
-                alt='instituciones'
-              />
+    <>
+      <section className='blog-page'>
+        <div className='container'>
+          <div className='col-md-8 mx-auto'>
+            <div className='post-item'>
+              <div className='post-item-wrap'>
+                <div className='post-item-desc text-center py-0'>
+                  <h3>Instituciones Participantes</h3>
+                </div>
+
+                <div className='post-image  testi-full '>
+                  <img
+                    src='/images/instituciones/instituciones.png'
+                    alt='instituciones'
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
